ce5f5f232c054cd7f927d315f9757a0bee53a15b
west1636 <nishikawa@nii.ac.jp>
2021-02-26 17:00:21 +0900

    revise Microsoft account error display(4)

--- addons/integromat/views.py
+++ addons/integromat/views.py
@@ -361,15 +361,15 @@ def integromat_add_microsoft_teams_user(**kwargs):
     nodeNum = nodeSettings.id
     if models.Attendees.objects.filter(node_settings_id=nodeNum, user_guid=userGuid).exists():
         logger.info('user_guid duplicate.')
-        raise HTTPError('User guid is duplicate', response=self)
+        raise HTTPError(http_status.HTTP_400_BAD_REQUEST)
 
-    if models.Attendees.objects.filter(node_settings_id=nodeNum, microsoft_teams_user_object=microsoftTeamsUserObject).exists():
-        logger.info('Microsoft User Object ID duplicate.')
-        raise HTTPError('Microsoft User Object ID duplicate.', response=self)
+#    if models.Attendees.objects.filter(node_settings_id=nodeNum, microsoft_teams_user_object=microsoftTeamsUserObject).exists():
+#        logger.info('Microsoft User Object ID duplicate.')
+#        raise HTTPError('Microsoft User Object ID duplicate.', response=self)
 
-    if models.Attendees.objects.filter(node_settings_id=nodeNum, microsoft_teams_mail=microsoftTeamsMail).exists():
-        logger.info('Microsoft Teams Sign-in Address duplicate.')
-        raise HTTPError('Microsoft Teams Sign-in Address duplicate.', response=self)
+#    if models.Attendees.objects.filter(node_settings_id=nodeNum, microsoft_teams_mail=microsoftTeamsMail).exists():
+#        logger.info('Microsoft Teams Sign-in Address duplicate.')
+#        raise HTTPError('Microsoft Teams Sign-in Address duplicate.', response=self)
 
     microsoftTeamsUserInfo = models.Attendees(
         user_guid = userGuid,
