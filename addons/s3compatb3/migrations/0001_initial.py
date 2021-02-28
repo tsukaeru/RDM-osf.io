1949ba77f98b038f10d2ba7e60fa301a2c8295ee
hide24 <hidetoshi.yoshimoto@gmail.com>
2021-03-01 07:33:40 +0900

    test

--- addons/s3compatb3/migrations/0001_initial.py
+++ addons/s3compatb3/migrations/0001_initial.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
+import django_extensions.db.fields
 import osf.models.base
 import osf.utils.datetime_aware_jsonfield
 
@@ -23,6 +24,8 @@ class Migration(migrations.Migration):
             name='NodeSettings',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created', django_extensions.db.fields.CreationDateTimeField(auto_now_add=True, verbose_name='created')),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(auto_now=True, verbose_name='modified')),
                 ('_id', models.CharField(db_index=True, default=osf.models.base.generate_object_id, max_length=24, unique=True)),
                 ('deleted', models.BooleanField(default=False)),
                 ('folder_id', models.TextField(blank=True, null=True)),
@@ -39,6 +42,8 @@ class Migration(migrations.Migration):
             name='UserSettings',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created', django_extensions.db.fields.CreationDateTimeField(auto_now_add=True, verbose_name='created')),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(auto_now=True, verbose_name='modified')),
                 ('_id', models.CharField(db_index=True, default=osf.models.base.generate_object_id, max_length=24, unique=True)),
                 ('deleted', models.BooleanField(default=False)),
                 ('oauth_grants', osf.utils.datetime_aware_jsonfield.DateTimeAwareJSONField(blank=True, default=dict, encoder=osf.utils.datetime_aware_jsonfield.DateTimeAwareJSONEncoder)),
@@ -52,28 +57,6 @@ class Migration(migrations.Migration):
             model_name='nodesettings',
             name='user_settings',
             field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='addons_s3compatb3.UserSettings'),
-        ),
-        migrations.AddField(
-            model_name='nodesettings',
-            name='created',
-            field=django_extensions.db.fields.CreationDateTimeField(auto_now_add=True, default=django.utils.timezone.now, verbose_name='created'),
-            preserve_default=False,
-        ),
-        migrations.AddField(
-            model_name='nodesettings',
-            name='modified',
-            field=django_extensions.db.fields.ModificationDateTimeField(auto_now=True, verbose_name='modified'),
-        ),
-        migrations.AddField(
-            model_name='usersettings',
-            name='created',
-            field=django_extensions.db.fields.CreationDateTimeField(auto_now_add=True, default=django.utils.timezone.now, verbose_name='created'),
-            preserve_default=False,
-        ),
-        migrations.AddField(
-            model_name='usersettings',
-            name='modified',
-            field=django_extensions.db.fields.ModificationDateTimeField(auto_now=True, verbose_name='modified'),
         ),
         migrations.AddField(
             model_name='nodesettings',
