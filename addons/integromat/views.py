9ac32ea2ed4a6404633393ec9a8440d1f1e6ecab
west1636 <nishikawa@nii.ac.jp>
2021-03-24 19:21:33 +0900

    revise views.py

--- addons/integromat/views.py
+++ addons/integromat/views.py
@@ -211,7 +211,7 @@ def integromat_api_call(*args, **kwargs):
     logger.info('auth:' + str(auth))
     logger.info('auth:' + str(auth.user))
 
-    return {'user': auth.user}
+    return {'user': str(auth.user)}
 
 @must_be_logged_in
 def integromat_create_meeting_info(**kwargs):
