82752d60670746431f1bee6d40d0f4a213338d81
west1636 <nishikawa@nii.ac.jp>
2021-05-26 22:17:26 +0900

    revise views.py

--- addons/integromat/views.py
+++ addons/integromat/views.py
@@ -209,15 +209,20 @@ def integromat_set_config_ember(**kwargs):
     upcomingWebMeetings = models.AllMeetingInformation.objects.filter(node_settings_id=addon.id, start_datetime__gte=datetime.today()).order_by('start_datetime')
     previousWebMeetings = models.AllMeetingInformation.objects.filter(node_settings_id=addon.id, start_datetime__lt=datetime.today()).order_by('start_datetime').reverse()
 
+    nodeWebMeetingsAttendeesRelation = models.AllMeetingInformationAttendeesRelation.objects.filter(all_meeting_information__node_settings_id=addon.id)
+
     allWebMeetingsJson = serializers.serialize('json', allWebMeetings, ensure_ascii=False)
     upcomingWebMeetingsJson = serializers.serialize('json', upcomingWebMeetings, ensure_ascii=False)
     previousWebMeetingsJson = serializers.serialize('json', previousWebMeetings, ensure_ascii=False)
+    nodeWebMeetingsAttendeesRelationJson = serializers.serialize('json', nodeWebMeetingsAttendeesRelation, ensure_ascii=False)
+
 
     return {'data': {'id': node._id, 'type': 'integromat-config',
                      'attributes': {
                          'all_web_meetings': allWebMeetingsJson,
                          'upcoming_web_meetings': upcomingWebMeetingsJson,
-                         'previous_web_meetings': previousWebMeetingsJson
+                         'previous_web_meetings': previousWebMeetingsJson,
+                         'node_web_meetings_attendees_relation': nodeWebMeetingsAttendeesRelationJson
                      }}}
 
 # ember: ここまで
