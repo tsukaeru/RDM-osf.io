aecc7fb520fda4057ef67c37a5dac1236832d88b
west1636 <nishikawa@nii.ac.jp>
2021-06-10 12:59:06 +0900

    delete migrationfile 0180_rdmworkflows_webhook_url, revise rdm_integromat.py

--- osf/models/rdm_integromat.py
+++ osf/models/rdm_integromat.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from osf.utils.fields import EncryptedTextField
 
 class RdmWorkflows(models.Model):
     id = models.AutoField(primary_key=True)
     workflow_description = models.CharField(max_length=256, unique=True)
     workflow_apps = models.CharField(max_length=256, unique=True)
-    webhook_url = EncryptedTextField(blank=True, null=True)
 
 class RdmWebMeetingApps(models.Model):
     id = models.AutoField(primary_key=True)
