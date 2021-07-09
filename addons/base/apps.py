772d8239eeda93394eac1f53484c061bcc02dfdf
west1636 <nishikawa@nii.ac.jp>
2021-07-09 16:42:28 +0900

    Revert "admin setting is_allowed_default False for integromat addon"
    This reverts commit ce09f9377bd60349ecd748996532aa200c524acc.

--- addons/base/apps.py
+++ addons/base/apps.py
@@ -76,7 +76,8 @@ class BaseAddonAppConfig(AppConfig):
     tab_name = ''
 
     # default value for RdmAddonOption.is_allowed for GRDM Admin
-    is_allowed_default = False
+    is_allowed_default = True
+    for_institutions = False
 
     def __init__(self, *args, **kwargs):
         ret = super(BaseAddonAppConfig, self).__init__(*args, **kwargs).__init__()
