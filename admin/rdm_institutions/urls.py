d8005ffaa63f478332bd2b47715abb89daab40db
hide24 <hidetoshi.yoshimoto@gmail.com>
2021-09-14 04:08:47 +0900

    change link href

--- admin/rdm_institutions/urls.py
+++ admin/rdm_institutions/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.InstitutionDetail.as_view(), name='institution_detail'),
+    url(r'^$', views.InstitutionDetail.as_view(), name='detail'),
     # url(r'^logo$', views.LogoUpload.as_view(), name='logo_upload'),
     # url(r'^banner$', views.BannerUpload.as_view(), name='banner_upload'),
 ]
