4307bc5b9b05ac587d4ea215d609c6a97a7bc45d
west1636 <nishikawa@nii.ac.jp>
2021-09-15 17:25:23 +0900

    change detail settings for rdm_grdmapps

--- osf/models/rdm_grdmapps.py
+++ osf/models/rdm_grdmapps.py
@@ -5,9 +5,9 @@ from osf.models.base import BaseModel, ObjectIDMixin
 
 class RdmWorkflows(ObjectIDMixin, BaseModel):
 
-    workflow_description = models.CharField(max_length=256, unique=True)
-    workflow_apps = models.CharField(max_length=256, unique=True)
+    workflow_description = models.CharField(max_length=255, unique=True)
+    workflow_apps = models.CharField(max_length=255, unique=True)
 
 class RdmWebMeetingApps(ObjectIDMixin, BaseModel):
 
-    app_name = models.CharField(max_length=128, unique=True)
+    app_name = models.CharField(max_length=255, unique=True)
