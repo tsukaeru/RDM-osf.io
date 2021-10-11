079cdc5c3bda83d22300bf1db62a3808b6f85b64
west1636 <nishikawa@nii.ac.jp>
2021-10-12 00:10:05 +0900

    remove webhook_url from get_data

--- addons/integromat/views.py
+++ addons/integromat/views.py
@@ -618,10 +618,11 @@ def integromat_start_scenario(**kwargs):
     requestData = request.get_data()
     requestDataJson = json.loads(requestData)
     timestamp = requestDataJson['timestamp']
-
     webhook_url = requestDataJson['webhook_url']
 
-    response = requests.post(webhook_url, data=request.get_data(), headers={'Content-Type': 'application/json'})
+    requestData = request.get_data().pop('webhook_url')
+
+    response = requests.post(webhook_url, data=requestData, headers={'Content-Type': 'application/json'})
 
     logger.info('webhook response:' + str(response))
     logger.info('integromat_start_scenario end')
