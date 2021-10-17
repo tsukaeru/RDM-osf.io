abf8a3251fd751d3d7b13eff484ac15c389de840
Wiktor Klonowski <wiktor.klonowski@gmail.com>
2021-10-18 08:50:47 +0900

    Add remaining base files

--- addons/rushfiles/models.py
+++ addons/rushfiles/models.py
@@ -5,8 +5,10 @@ from addons.base.models import (BaseOAuthNodeSettings, BaseOAuthUserSettings,
                                 BaseStorageAddon)
 from django.db import models
 
+from framework.exceptions import HTTPError
 from osf.models.external import ExternalProvider
 from osf.models.files import File, Folder, BaseFileNode
+from addons.base import exceptions
 from addons.rushfiles import settings
 from addons.rushfiles.serializer import RushFilesSerializer
 
@@ -31,8 +33,12 @@ class RushFilesProvider(ExternalProvider):
     client_id = settings.CLIENT_ID
     client_secret = settings.CLIENT_SECRET
 
+
     auth_url_base = '{}{}'.format(settings.OAUTH_BASE_URL, 'authorize')
     callback_url = '{}{}'.format(settings.OAUTH_BASE_URL, 'token')
+    auto_refresh_url = callback_url
+    refresh_time = 180
+    expiry_time = 0
 
     default_scopes = settings.OAUTH_SCOPE
 
@@ -65,6 +71,15 @@ class NodeSettings(BaseOAuthNodeSettings, BaseStorageAddon):
     domain = models.TextField(null=True, blank=True)
     user_settings = models.ForeignKey(UserSettings, null=True, blank=True, on_delete=models.CASCADE)
 
+    _provider = None
+
+    @property
+    def provider(self):
+        """Authenticated ExternalProvider instance"""
+        if self._provider is None:
+            self._provider = RushFilesProvider(self.external_account)
+        return self._provider
+
     @property
     def folder_id(self):
         return self.share_id or None
@@ -75,5 +90,65 @@ class NodeSettings(BaseOAuthNodeSettings, BaseStorageAddon):
 
     @property
     def folder_path(self):
-        return self.share_id or None
+        return self.share_name or None
+
+    def fetch_access_token(self):
+        return self.provider.fetch_access_token()
+
+    def get_folders(self, **kwargs):
+        node = self.owner
+
+        try:
+            access_token = self.fetch_access_token()
+        except exceptions.InvalidAuthError:
+            raise HTTPError(403)
+
+        #TODO Return the list of shares available to the authenticated user
+        # https://clientgateway.rushfiles.com/swagger/ui/index#!/User/User_GetUserShares
+        # Get main user domain from access token (JWT)
+
+        return [{
+            'addon': self.config.short_name,
+            'path': '開発',
+            'kind': 'folder',
+            'id': 'd0c475011bd24b6dae8a6f890f6b4a93',
+            'name': '開発',
+            'urls': {}
+        }]
+
+    def set_folder(self, folder, auth):
+        """Configure this addon to point to a Google Drive folder
+
+        :param dict folder:
+        :param User user:
+        """
+        self.share_id = folder['id']
+        self.share_name = folder['name']
+
+        # Tell the user's addon settings that this node is connecting
+        self.user_settings.grant_oauth_access(
+            node=self.owner,
+            external_account=self.external_account,
+            metadata={'folder': self.share_id}
+        )  # Performs a save on self.user_settings
+        self.save()
+
+        self.nodelogger.log('folder_selected', save=True)
     
+    # Match with RDM-waterbutler/waterbutler/providers/rushfiles/provider.py:RushFilesProvider::__init__
+    def serialize_waterbutler_credentials(self):
+        if not self.has_auth:
+            raise exceptions.AddonError('Addon is not authorized')
+        return {'token': self.fetch_access_token()}
+
+    # Match with RDM-waterbutler/waterbutler/providers/rushfiles/provider.py:RushFilesProvider::__init__
+    def serialize_waterbutler_settings(self):
+        if not self.folder_id:
+            raise exceptions.AddonError('Folder is not configured')
+
+        return {
+            'share': {
+                'id': self.folder_id,
+                'name': self.folder_name,
+            }
+        }
