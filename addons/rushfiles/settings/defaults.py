c26882a2d5f549bd53e86b558c459951eb05fed9
Wiktor Klonowski <wiktor.klonowski@gmail.com>
2021-10-19 11:18:31 +0900

    Fix for real oauth client

--- addons/rushfiles/settings/defaults.py
+++ addons/rushfiles/settings/defaults.py
@@ -1,6 +1,9 @@
 # Drive credentials
-CLIENT_ID = 'chaneme'
+CLIENT_ID = 'changeme'
 CLIENT_SECRET = 'changeme'
+
+EXPIRY_TIME = 60 * 60 * 24  # 24 hours
+REFRESH_TIME = 60 * 60 * 24 * 60  # 60 days
 
 OAUTH_SCOPE = ['openid', 'profile', 'domain_api', 'offline_access']
 OAUTH_BASE_URL = 'https://auth.rushfiles.com/connect/'
