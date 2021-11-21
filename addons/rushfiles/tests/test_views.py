da870bcdf1d8834481d720eae97e964e9a4677ec
kitakou0313 <kitakou0313@gmail.com>
2021-11-21 16:31:46 +0900

    Add test for config view

--- addons/rushfiles/tests/test_views.py
+++ addons/rushfiles/tests/test_views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import mock
 
 from addons.base.tests.views import (
@@ -7,18 +8,27 @@ from tests.base import OsfTestCase
 from addons.rushfiles.tests.utils import RushfilesAddonTestCase
 from addons.rushfiles.client import RushFilesClient
 from addons.rushfiles.serializer import RushFilesSerializer
+from nose.tools import assert_equal
+assert_equal.__self__.maxDiff = None
 
 class TestRushfilesConfigViews(RushfilesAddonTestCase, OAuthAddonConfigViewsTestCaseMixin, OsfTestCase):
-    Serializer = RushFilesSerializer
-    client = RushFilesClient
     folder = {
         'path': 'fake_share',
         'id': 'abcdef@fake.net',
         'name':'fake_share'
     }
+    Serializer = RushFilesSerializer
+    client = RushFilesClient
 
     def setUp(self):
         super(TestRushfilesConfigViews, self).setUp()
+
+    @mock.patch('addons.rushfiles.models.NodeSettings.fetch_access_token')
+    def test_get_config(self, mock_access_token):
+        mock_access_token.return_value = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJwcmltYXJ5X2RvbWFpbiI6ImZha2VAZmFrZS5uZXQifQ._CTx5dIZ0piHbqnF63NV-G6nuFs9uN-9q-pnR0X5HYE"
+        super(TestRushfilesConfigViews, self).test_get_config()
+
+
 
     @mock.patch.object(RushFilesClient, 'shares')
     @mock.patch('addons.rushfiles.models.NodeSettings.fetch_access_token')
