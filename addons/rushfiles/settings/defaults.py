9ac5ff387f146cbdedbf0593d60afbc502ac42f3
Wiktor Klonowski <wiktor.klonowski@gmail.com>
2022-04-12 20:02:01 +0200

    Update access token lifetime

--- addons/rushfiles/settings/defaults.py
+++ addons/rushfiles/settings/defaults.py
@@ -3,7 +3,7 @@ CLIENT_ID = 'changeme'
 CLIENT_SECRET = 'changeme'
 
 EXPIRY_TIME = 60 * 60 * 24 * 60  # 60 days
-REFRESH_TIME = 60 * 60 * 12  # 12 hours (token is valid for 24 hours)
+REFRESH_TIME = 60 * 2.5  # 2.5 minutes (by default, token is valid for 5 minutes)
 
 OAUTH_SCOPE = ['openid', 'profile', 'domain_api', 'offline_access']
 OAUTH_BASE_URL = 'https://auth.rushfiles.com/connect/'
